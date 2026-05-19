97eb55422466316b2f37fe411b623c76dd549e57
ABHEET SHARMA <abheetsharma09@gmail.com>
2026-05-19 19:29:41 +0530

    Basic Hello World Program in C++

---
+++ 01_Basics/Hello_World.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //Must include this line (preprocessor directive)
+using namespace std; //either use this or std::
+
+int main(){
+    cout << "Hello World..\n";
+    return 0; //As int is the function so it must return something
+}
