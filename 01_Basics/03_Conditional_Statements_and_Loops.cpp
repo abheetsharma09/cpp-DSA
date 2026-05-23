c1bce9c0b9977b491f58cdd7078203db6c7d0455
ABHEET SHARMA <abheetsharma09@gmail.com>
2026-05-23 22:48:50 +0530

    Added Conditional Statements and Loops

---
+++ 01_Basics/03_Conditional_Statements_and_Loops.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+////////////////////////////////////CONDITIONAL STATEMENTS////////////////////////////////////
+
+//AGE ADULT OR Not
+// int main(){
+//     int age;
+//     std::cout << "Enter your Age : ";
+//     std::cin >> age;
+//     if (age >= 18){
+//         std::cout << "Adult Human";
+//     }else if (age <= 0)
+//     {
+//         std::cout << "ERROR!!";
+//     }else {
+//         std::cout << "Not Adult";
+//     }
+//     return 0;
+// }
+
+
+
+//ODD EVEN FUNCTION
+// int main (){
+//     int num;
+//     std::cout << "Enter a Number : ";
+//     std::cin >> num;
+//     if (num % 2 == 0){
+//         std::cout << "EVEN\n";
+//     } else{
+//         std::cout << "ODD";
+//     }
+    
+// }
+
+
+//find if the Value is in uppercase or not
+// int main(){
+//     char main_char;
+//     std::cout << "Enter Letter : ";
+//     std::cin >> main_char;
+//     int new_main_char = main_char;
+
+//     if (new_main_char >= 65 && new_main_char <= 95){
+//         std::cout << "A Capital Letter\n";
+//     } else if (new_main_char >= 97 && new_main_char <= 122){
+//         std::cout << "A Small Letter\n";
+//     }    
+// }
+
+/////////////////////////LOOPS///////////////////////////////
+//While Loops
+// int main(){
+//     int num = 0;
+//     while (num != 25)
+//     {
+//         num ++;
+//         std::cout << num << std::endl;
+//     }
+// }
+
+//For Loops
