c62d26ed4fb40765ceb3bf5b758b6704bd373639
ABHEET SHARMA <abheetsharma09@gmail.com>
2026-05-24 21:02:27 +0530

    Added Pattern.cpp | Nested Loops

---
+++ 01_Basics/04_Patterns.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // FOR LOOPS - Initialization , Condition ,Updation
+
+    // Question - 01[Square Patterns]
+
+    int count = 4;
+    int times = 0;
+    for (int j = 0; j != count; j++)
+    {
+        for (int i = 1; i != count + 1; i++)
+        {
+            cout << i;
+        }
+        cout << "\n";
+        times++;
+    }
+
+    //Question - 02
+return 0;
+}
