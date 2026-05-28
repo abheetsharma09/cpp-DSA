5d264abfe93629818f6faff8ea8d5ada6a302b4f
ABHEET SHARMA <abheetsharma09@gmail.com>
2026-05-28 21:51:15 +0530

    Added Max SubArray Sum Problem | Kadane's Algorithm

---
+++ 02_Arrays/10_Kadane's_Algo_Max_SubArray_Sum.cpp
@@ -0,0 +1,88 @@
+// MAXIMUM SUBARRAY SUM | KADANE's ALGORITHM
+#include <iostream>
+#include <vector>
+using namespace std;
+
+///////////////////////////////By BRUTE FORCE APPROACH//////////////////////////////////
+int maxSubArraySum()
+{
+    // Here in this array {2,3,4}, {2,3},{4,5} is a Subarray.
+    vector<int> vec = {1, 2, 3, 4, 5};
+    vector<int> sumSUBarray;
+
+    // Number of Subarray Formulae = (n*(n+1))/2
+    int highestSubArraySUM = 0;
+    int vecSize = vec.size();                                // Size of the Vector Array
+    int vecSubArrayFormulae = (vecSize * (vecSize + 1)) / 2; // How many Subarray
+
+    // List every possiblity of Subarray
+    for (short int i = 0; i < vecSize; i++)
+    {
+        for (short int j = 0; j < vecSize; j++)
+        {
+            if (i == j)
+            {
+                cout << "[" << vec[i] << "]" << endl;
+            }
+            else if (i != j && j > i)
+            {
+                int maxSubArraySum = 0;
+                cout << "[";
+                // cout << "[" << vec[i] << "," << vec[j] << "]" << endl;
+                for (short int k = i; k < j + 1; k++)
+                {
+                    // Brute Force Approach to find the Maximum Subarray Sum
+                    // 1.> We sum all the numbers of Sub Array
+                    maxSubArraySum += vec[k];
+
+                    cout << vec[k] << ","; // Outputs the SubArray possibility
+                }
+                cout << "]" << endl;
+
+                // 2.> Push every sumed up value to the Vector Array
+                sumSUBarray.push_back(maxSubArraySum);
+            }
+        }
+        cout << endl;
+    }
+
+    // Find the Highest Number in the Vector Array
+    for (int i : sumSUBarray)
+    {
+        // Maximum Subarray Sum
+        if (i > highestSubArraySUM)
+        {
+            highestSubArraySUM = i;
+        }
+    }
+
+    cout << "Highest SUB Array Sum : " << highestSubArraySUM;
+    return highestSubArraySUM;
+}
+
+////////////////////////////By KADANE's ALGORITHM//////////////////////////////
+int kadaneAlgoritm()
+{
+    vector<int> vec = {3, -4, 5, 4, -1, 7, -8};
+    int maxSum = INT_MIN; // Set it to the -infinity
+    int currSum = 0;
+    for (int i : vec)
+    {
+        currSum += (i);
+        maxSum = max(currSum, maxSum); // Returns the max value out of both
+        if (currSum < 0)
+        {
+            currSum = 0;
+        }
+    }
+
+    cout << maxSum;
+    return maxSum;
+}
+
+int main()
+{
+    kadaneAlgoritm();
+    // maxSubArraySum();
+    return 0;
+}
